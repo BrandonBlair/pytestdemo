bf2f768de9e74d6839801f915b43059a3c2574f8
Brandon Blair <cbrandon.blair@gmail.com>
2021-07-30 22:02:47 -0500

    Initial

---
+++ conftest.py
@@ -0,0 +1,6 @@
+from pytest import fixture
+
+@fixture
+def email():
+    return "brandon@techstepacademy.com"
+    
---
+++ test_login.py
@@ -0,0 +1,4 @@
+def test_can_login_using_api_request(email):
+    print(email)
+    expected_email = "brandon@techstepacademy.com"
+    assert email == expected_email
