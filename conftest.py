8f3d835d21440a40230cdb3572a030dd843fb84f
Brandon Blair <cbrandon.blair@gmail.com>
2021-08-08 12:43:31 -0500

    Initial

--- conftest.py
+++ conftest.py
@@ -1,6 +1,22 @@
 from pytest import fixture
+from requests import Session
 
 @fixture
-def email():
-    return "brandon@techstepacademy.com"
-    
+def logged_in_session():
+    session = Session()
+
+    # Mimic navigating to the landing page
+    landing_page_resp = session.get(url="http://127.0.0.1:5000/")
+
+    # Login with a POST
+    login_info = {
+        "email": "brandon@techstepacademy.com",
+        "password": "1234"
+    }
+
+    login_resp = session.post(url="http://127.0.0.1:5000/v1/login", data=login_info)
+
+    if login_resp.status_code != 200:
+        raise ValueError(f"Expected 200, but received {login_resp.status_code}")
+
+    return session
--- test_login.py
+++ test_login.py
@@ -1,4 +1,3 @@
-def test_can_login_using_api_request(email):
-    print(email)
-    expected_email = "brandon@techstepacademy.com"
-    assert email == expected_email
+def test_can_reach_search_with_logged_in_session(logged_in_session):
+    search_resp = logged_in_session.get(url="http://127.0.0.1:5000/search")
+    assert "Search for a book!" in search_resp.text
